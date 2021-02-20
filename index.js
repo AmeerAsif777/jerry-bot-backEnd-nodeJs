cb3812546c3e5ed2617589d4218db8fe50d367fb
Ameer Ashif <ameer.asif777@gmail.com>
2021-02-20 14:51:36 +0530

    Uncommented the connection url and added extra params

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use("/user", userRouter);
 const CONNECTION_URL = 'mongodb+srv://mernDemo:mernDemo@cluster0.uo9wk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL || CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
