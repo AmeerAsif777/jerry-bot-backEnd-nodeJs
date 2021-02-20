4412dacdf3b8e38c1894c5f8a14a96687ebcc0e1
Ameer Ashif <ameer.asif777@gmail.com>
2021-02-20 14:48:51 +0530

    Uncommented the connection url

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-// const CONNECTION_URL = 'mongodb+srv://mernDemo:mernDemo@cluster0.uo9wk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://mernDemo:mernDemo@cluster0.uo9wk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
