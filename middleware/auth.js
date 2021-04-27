c6a7f1c5b4e3520264b4c0fd47a8488738b7f205
Ameer Ashif <ameer.asif777@gmail.com>
2021-04-27 22:21:45 +0530

    Feature: Added comments model and format files with default prettier code formating

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,20 @@
 import jwt from "jsonwebtoken";
 
-const secret = 'test';
+const secret = "test";
 
 const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const isCustomAuth = token.length < 500;
-    console.log('Token', token);
     let decodedData;
 
-    if (token && isCustomAuth) {      
+    if (token && isCustomAuth) {
       decodedData = jwt.verify(token, secret);
-      console.log('decodedData', decodedData);
       req.userId = decodedData && decodedData.id;
     } else {
       decodedData = jwt.decode(token);
-      console.log('Google decodedData', decodedData);
       req.userId = decodedData && decodedData.sub;
-    }    
+    }
 
     next();
   } catch (error) {
